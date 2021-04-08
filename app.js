6e7d57577b3845e4c019317a37c8a6cb083a5d2b
martinchang95 <martinchang@hotmail.com>
2021-04-08 09:48:12 -0300

    Segundo commit

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
-app.listen(port, () => {
-    console.log('Example app listening on port '+ port);
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Servidor corriendo en el puerto 3000');
+})
